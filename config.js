423a65e3e75eff30a9014f383e46998899d49943
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-04-06 11:09:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    apiId: 20335961,
-    apiHash: "e34cc60ae543f62297f64ea93d8c6be6",
-    stringSession: "1AgAOMTQ5LjE1NC4xNjcuNDEBu0UamiLcQlEJ2Lo2OjNXqmbbDNQrW3+9WGC1adbDMMd4t8HQCRemjVm7Ozoa1ZrsahNBBMqCGATvUr2jknVOqnAdZvl0+DrrBBpOy3IaONibxMNZzvI8ljE/NAiUtUhZvF4xkvzpEzexc/gxXs2Usliraaw6aljs4bMfTDo1u0wHLeKjHrhFhlESuRZg6f5sMiULeF+XxlUhVJ2JM9FvLoqBIK4kjdTSBpStw36GzToaWh4/NTzHNu8jYgdRSVGR6KyDDnLWNlaIzsJfz7kyDhNSBXxSJDihL3SUXbaOvbKOIh2BGkpTP/Bk2KTVMRrRzGIFitm9FQOm7szhXZeBEnI=", // если есть сохранённая строка — вставь сюда
+    apiId: 123123,
+    apiHash: "",
+    stringSession: "", // если есть сохранённая строка — вставь сюда
   };
